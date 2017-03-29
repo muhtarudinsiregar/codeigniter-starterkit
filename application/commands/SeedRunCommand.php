b492540675951c3ce3991e3ee5307d333fbbe30a
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-03-29 20:51:08 +0700

    Register seed:run command

--- application/commands/SeedRunCommand.php
+++ application/commands/SeedRunCommand.php
@@ -6,7 +6,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class SeedRunCommand extends Commmand
+class SeedRunCommand extends Command
 {
     protected function configure()
     {
